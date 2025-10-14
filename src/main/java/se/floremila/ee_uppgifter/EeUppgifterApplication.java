9246141f71ca339d353e913d013bb3f1cb966524
Floremila <flormequi@gmail.com>
2025-10-14 12:03:14 +0200

    chore: base Maven project with JDK 8 (no dependencies)

---
+++ src/main/java/se/floremila/ee_uppgifter/EeUppgifterApplication.java
@@ -0,0 +1,13 @@
+package se.floremila.ee_uppgifter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EeUppgifterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EeUppgifterApplication.class, args);
+	}
+
+}

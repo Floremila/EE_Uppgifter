2e6d19336dab3d5121107adbbc53ab428276662c
Floremila <flormequi@gmail.com>
2025-10-22 15:31:42 +0200

    Uppgift #7: Select service via Spring profiles (dev=inMemory, prod=JPA) and remove @Primary

--- src/main/java/se/floremila/ee_uppgifter/lektion1/student/service/InMemoryStudentService.java
+++ src/main/java/se/floremila/ee_uppgifter/lektion1/student/service/InMemoryStudentService.java
@@ -1,7 +1,9 @@
 package se.floremila.ee_uppgifter.lektion1.student.service;
 
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
+@Profile("dev")
 @Service("inMemoryStudentService")
 public class InMemoryStudentService implements StudentService {
 

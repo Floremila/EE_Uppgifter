35d2b74237ba0a815c3b103c5792910012f339f4
Floremila <flormequi@gmail.com>
2025-10-27 10:29:58 +0100

    Lektion 2 — Uppgift #6: Implement Duck model and WebClient call returning Mono<Duck>

--- src/main/java/se/floremila/ee_uppgifter/lektion2/duck/model/Duck.java
+++ src/main/java/se/floremila/ee_uppgifter/lektion2/duck/model/Duck.java
@@ -1,5 +1,19 @@
 package se.floremila.ee_uppgifter.lektion2.duck.model;
 
 public class Duck {
+    private String url;
+    private String message;
 
+    public Duck() { }
+
+    public Duck(String url, String message) {
+        this.url = url;
+        this.message = message;
+    }
+
+    public String getUrl() { return url; }
+    public void setUrl(String url) { this.url = url; }
+
+    public String getMessage() { return message; }
+    public void setMessage(String message) { this.message = message; }
 }
